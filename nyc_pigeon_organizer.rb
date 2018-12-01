55203a7ea395b6bae2ab0206bf5f4a8f0ca0d40d
Wil Cook <wil.cook.is@gmail.com>
2018-12-01 23:31:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "pry"
+
+
+# delete_if for reference
+# h = { "a" => 100, "b" => 200, "c" => 300 }
+# h.delete_if {|key, value| key >= "b" }   #=> {"a"=>100}
+
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |attribute, pigeon|
+      # binding.pry
+      i = 0
+      while i < data.length
+        pigeon_list = pigeon_list.merge({pigeon[i] => {}})
+        # binding.pry
+        i +=1
+      end
+    end
+  end
+  pigeon_list.delete_if { |key, value| key == nil }
+  binding.pry
+  
 end
