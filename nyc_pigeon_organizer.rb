bdd139b68d828ef37b6026b60acfaa23c9770871
Wil Cook <wil.cook.is@gmail.com>
2018-12-02 00:24:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,13 +15,25 @@ def nyc_pigeon_organizer(data)
       # binding.pry
       i = 0
       while i < data.length
-        pigeon_list = pigeon_list.merge({pigeon[i] => {}})
+        pigeon_list = pigeon_list.merge({pigeon[i] => {:color => [], :gender => [], :lives=> []}})
         # binding.pry
         i +=1
       end
     end
   end
   pigeon_list.delete_if { |key, value| key == nil }
-  binding.pry
-  
+  # binding.pry
+  data.each do |key, value|
+    # binding.pry
+    pigeon_list.each do |pigeon, attribute|
+      # binding.pry
+      value.each do |trait, pigeon_array|
+        # binding.pry
+        if pigeon_array.any? {|word| word == pigeon}
+          attribute[key] << trait.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
